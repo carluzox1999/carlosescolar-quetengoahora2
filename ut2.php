c0606919acd43df150fc1f5a80990633aa4cb956
Carlos Escolar Calero <carlosescolarcalero@alumno.ieselrincon.es>
2022-09-21 11:08:28 +0100

    Update ut2.php

--- ut2.php
+++ ut2.php
@@ -15,52 +15,97 @@
                     "Materia" => "EMR",
                     "Aula" => "201"
                 ),
-                "08:55-10:45" => array(
+                "08:55-09:50" => array(
                     "Docente" => "Sergio Ramos Suarez",
                     "Materia" => "DSW", 
                     "Aula" => "201"
                 ),
-                "11:15-14:00" => array(
+                "09:50-10:45" => array(
+                    "Docente" => "Sergio Ramos Suarez",
+                    "Materia" => "DSW", 
+                    "Aula" => "201"
+                ),
+                "11:15-12:10" => array(
                     "Docente" => "Maria del Carmen Rodriguez Suarez", 
                     "Materia" => "DEW", 
                     "Aula" => "201"
-                )              
+                ),   
+                "12:10-13:05" => array(
+                    "Docente" => "Maria del Carmen Rodriguez Suarez", 
+                    "Materia" => "DEW", 
+                    "Aula" => "201"
+                ),  
+                "13:05-14:00" => array(
+                    "Docente" => "Maria del Carmen Rodriguez Suarez", 
+                    "Materia" => "DEW", 
+                    "Aula" => "201"
+                ),              
             ),
 
             "Martes" =>  array(
-                "08:00-09:50" => array(
+                "08:00-08-55" => array(
                     "Docente" => "Maria Antonia Montesdeoca Viera", 
-                    "Materia" => "DEW",
+                    "Materia" => "DPL",
                     "Aula" => "201"
                 ),
-                "09:50-12:10" => array(
+                "08:55-09:50" => array(
+                    "Docente" => "Maria Antonia Montesdeoca Viera", 
+                    "Materia" => "DPL",
+                    "Aula" => "201"
+                ),
+                "09:50-10:45" => array(
                     "Docente" => "Sergio Ramos Suarez", 
                     "Materia" => "DSW",
                     "Aula" => "201"
                 ),
-                "12:10-14:00" => array(
+                "11:15-12:10" => array(
+                    "Docente" => "Sergio Ramos Suarez", 
+                    "Materia" => "DSW",
+                    "Aula" => "201"
+                ),
+                "12:10-13:05" => array(
                     "Docente" => "Ermis Papakonstantinou Baez", 
                     "Materia" => "DOR",
                     "Aula" => "201"
-                )             
+                ),
+                "13:05-14:00" => array(
+                    "Docente" => "Ermis Papakonstantinou Baez", 
+                    "Materia" => "DOR",
+                    "Aula" => "201"
+                ),            
             ),
 
             "Miercoles" =>  array(
-                "08:00-09:50" => array(
+                "08:00-08:55" => array(
                     "Docente" => "Maria del Carmen Rodriguez Suarez",
                     "Materia" => "DEW", 
                     "Aula" => "201"
                 ),
-                "09:50-12:10" => array(
+                "08:55-09:50" => array(
+                    "Docente" => "Maria del Carmen Rodriguez Suarez",
+                    "Materia" => "DEW", 
+                    "Aula" => "201"
+                ),
+                "09:50-10:45" => array(
                     "Docente" => "Sergio Ramos Suarez", 
                     "Materia" => "DSW",
                     "Aula" => "201"
                 ),
-                "12:10-14:00" => array(
+                "11:15-12:10" => array(
+                    "Docente" => "Sergio Ramos Suarez", 
+                    "Materia" => "DSW",
+                    "Aula" => "201"
+                ),
+                "12:10-13:05" => array(
                     "Docente" => "Ermis Papakonstantinou Baez", 
                     "Materia" => "DOR",
                     "Aula" => "201"
-                )          
+                ), 
+                "13:05-14:00" => array(
+                    "Docente" => "Ermis Papakonstantinou Baez", 
+                    "Materia" => "DOR",
+                    "Aula" => "201"
+                ),        
             ),
 
             "Jueves" =>  array(
@@ -69,7 +114,22 @@
                     "Materia" => "DPL",
                     "Aula" => "201"
                 ),
-                "11:15-13:05" => array(
+                "08:55-09:50" => array(
+                    "Docente" => "Maria Antonia Montesdeoca Viera", 
+                    "Materia" => "DPL",
+                    "Aula" => "201"
+                ),
+                "09:50-10:45" => array(
+                    "Docente" => "Maria Antonia Montesdeoca Viera", 
+                    "Materia" => "DPL",
+                    "Aula" => "201"
+                ),
+                "11:15-12:10" => array(
+                    "Docente" => "Maria del Carmen Rodriguez Suarez", 
+                    "Materia" => "DEW",
+                    "Aula" => "201"
+                ),
+                "12:10-13:05" => array(
                     "Docente" => "Maria del Carmen Rodriguez Suarez", 
                     "Materia" => "DEW",
                     "Aula" => "201"
@@ -82,7 +142,12 @@
             ),
 
             "Viernes" =>  array(
-                "08:00-09:50" => array(
+                "08:00-08:55" => array(
+                    "Docente" => "Maria Antonia Montesdeoca Viera", 
+                    "Materia" => "DOR",
+                    "Aula" => "201"
+                ),
+                "08:55-09:50" => array(
                     "Docente" => "Maria Antonia Montesdeoca Viera", 
                     "Materia" => "DOR",
                     "Aula" => "201"
@@ -97,11 +162,16 @@
                     "Materia" => "EMR", 
                     "Aula" => "201"
                 ),
-                "12:10-14:00" => array(
+                "12:10-13:05" => array(
                     "Docente" => "Sergio Ramos Suarez", 
                     "Materia" => "DSW", 
                     "Aula" => "201"
-                )        
+                ),
+                "13:05-14:00" => array(
+                    "Docente" => "Sergio Ramos Suarez", 
+                    "Materia" => "DSW", 
+                    "Aula" => "201"
+                )            
             )
         );
     
@@ -123,50 +193,46 @@
         
         function modulo($diaModulo, $horaModulo, $minModulo){
             global $horario;
-            $primeraHora = null;
+            // $primeraHora = null;
 
-            if ($horaModulo==8 && $minModulo<=55) {
-                $primeraHora = "08:00 - 08:55";
+            if ($horaModulo>=8 && $minModulo<=55) {
+                $primeraHora = "08:00-08:55";
             }
-            if($horaModulo==8 && $minModulo<=55 || $horaModulo==9 && $minModulo<=50 ){
-                $primeraHora = "08:55 - 09:50";
+            if($horaModulo>=8 && $minModulo<=55 || $horaModulo>=9 && $minModulo<=50 ){
+                $primeraHora = "08:55-09:50";
             }
-            if($horaModulo==9 && $minModulo<=50 || $horaModulo==10 && $minModulo<=45 ){
-                $primeraHora = "09:50 - 10:45";
+            if($horaModulo>=9 && $minModulo<=50 || $horaModulo>=10 && $minModulo<=45 ){
+                $primeraHora = "09:50-10:45";
             }
-            if($horaModulo==11 && $minModulo<=15 || $horaModulo==12 && $minModulo<=10){
-                $primeraHora = "11:15 - 12:10";
+            if($horaModulo>=11 && $minModulo<=15 || $horaModulo>=12 && $minModulo<=10){
+                $primeraHora = "11:15-12:10";
             }
-            if($horaModulo==12 && $minModulo<=10 || $horaModulo==13 && $minModulo<=05){
-                $primeraHora = "12:10 - 13:05";
+            if($horaModulo>=12 && $minModulo<=10 || $horaModulo>=13 && $minModulo<=05){
+                $primeraHora = "12:10-13:05";
             }
-            if($horaModulo==13 && $minModulo<=05 || $horaModulo==14 && $minModulo<=00){
-                $primeraHora = "13:05 - 14:00";
+            if($horaModulo>=13 && $minModulo<=05 || $horaModulo>=14 && $minModulo<=00){
+                $primeraHora = "13:05-14:00";
             }
 
             foreach ($horario as $dia => $programa) {
                 if ($dia == $diaModulo) {
-                    foreach ($programa as $segundaHora => $info) {
-                        if ($primeraHora === $segundaHora) {
+                    foreach ($programa as $primeraHora2 => $info) {
+                        if ($primeraHora == $primeraHora2) {
+                            echo "<h2>Nos toca...:</h2>";
                             foreach ($info as $nombre => $cont) {
-                              echo "$nombre: $cont";
+                                echo "</br>$nombre: $cont";
                             }
-                        }else{
-                            echo "No existe hora establecida";
                         }
                     }
-                }else{
-                  echo "No existe dia establecido";
                 }
             }
         };
 
-        mostrarHorario($horario);
-    ?>
-    <br>
-    <?php
+        
         
         modulo("Miercoles","8", "40");
+        echo "<hr>";
+        mostrarHorario($horario);
     ?>
 </body>
 </html>
